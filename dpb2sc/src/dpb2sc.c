5f08e990ad24765952868d99610c4c23027c0ca0
Borja Martínez Sánchez <bmarsan@upv.es>
2024-05-16 10:35:32 +0200

    Changed comment

--- dpb2sc/src/dpb2sc.c
+++ dpb2sc/src/dpb2sc.c
@@ -3048,7 +3048,7 @@ void sighandler(int signum) {
  *
  * @param char *uuid: String where UUID is stored
  *
- * @return 0 ans stores the UUID gnerated
+ * @return 0 and stores the UUID generated
  */
 int gen_uuid(char *uuid) {
     char v[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
