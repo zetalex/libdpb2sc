055e74321b725e9db49580c1d30137b59e04b464
Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>
2024-11-25 14:53:30 +0100

    Added guard to interface .h
    Signed-off-by: Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>

--- dpb2sc_Cpp/src/daq_inter_obj.h
+++ dpb2sc_Cpp/src/daq_inter_obj.h
@@ -1,4 +1,9 @@
+#ifndef __DAQ_INTER_OBJ_H_INCLUDED__
+#define __DAQ_INTER_OBJ_H_INCLUDED__
+
 #ifdef DAQ_MODE
     #include <daqinterface/DAQInterface.h>
 	extern ToolFramework::DAQInterface DAQ_Inter;
+#endif
+
 #endif
