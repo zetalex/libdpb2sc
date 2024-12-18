41ebc0c88399575f2aaba7890f062a8bd0b7cae0
Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>
2024-12-18 18:41:06 +0100

    Fix PLL Lock command
    Signed-off-by: Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>

--- dpb2sc_Cpp/src/dpb2sc.cpp
+++ dpb2sc_Cpp/src/dpb2sc.cpp
@@ -3359,10 +3359,10 @@ int dpb_command_handling(struct DPB_I2cSensors *data, char **cmd, int msg_id,cha
 			}
 			if(strcmp(cmd[2],"PLLLOCK") == 0){
 				//rc = poll_GPIO(pll_locked_fd,PLL_LOL_N,bool_read);
-				if(rc){
-					rc = command_status_response_json (msg_id,-ERRREAD,cmd_reply);
-					goto end;
-				}
+				// if(rc){
+				// 	rc = command_status_response_json (msg_id,-ERRREAD,cmd_reply);
+				// 	goto end;
+				// }
 				rc = command_status_response_json (msg_id,pll_locked_val,cmd_reply);
 				goto end;
 			}
