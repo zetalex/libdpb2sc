aa48fee7d3990fa0dabbaf522e8ecd21c9f0cf3a
Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>
2025-07-11 07:26:50 +0200

    Fix channel parsing in config
    Signed-off-by: Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>

--- dpb2sc_Cpp/src/dpb2sc.cpp
+++ dpb2sc_Cpp/src/dpb2sc.cpp
@@ -4411,42 +4411,80 @@ int config_parse(char *config_json){
 		json_object *jboard = NULL;
 		if (!json_object_object_get_ex(jobj, config_var.board, &jboard)){
 			LOG_PRINTF("The board %s is not present in the configuration file\n",config_var.board);
-			return -EINVAL;
+			continue;
 		}
 		json_object *jmag = NULL;
-		if(!json_object_object_get_ex(jboard,config_var.json_word,&jmag)) {
-			LOG_PRINTF("The word %s is not present in the board %s in the configuration file\n",config_var.json_word,config_var.board);
-			return -EINVAL;
-		}
+		char channel_array_name[12];
 		const char *mag_value;
-		mag_value = json_object_get_string(jmag);
-		if(mag_value == NULL) {
-			LOG_PRINTF("The value of %s in the board %s is not present in the configuration file\n",config_var.json_word,config_var.board);
-			return -EINVAL;
-		}
 		char cmd[64];
 		char *response;
-		if (config_var.env_chan == 0){
-			sprintf(cmd,"%s_%s",config_var.magnitude,mag_value);
-			response = command_parse(cmd);
-			if(strcmp(response,"OK")){
-				LOG_PRINTF("Error setting %s in the board %s\n",cmd,config_var.board);
+		if(config_var.env_chan == CHAN_PARAM){ //Channel parameter
+			if(!strcmp(config_var.board,"DPB")){
+				strcpy(channel_array_name,"SFPs");
 			}
 			else{
-				DEBUG_PRINTF_1("Setting %s in the board %s\n",cmd,config_var.board);
+				strcpy(channel_array_name,"channels");
 			}
-		}
-		else{
-			//Concatenate the channel number up to the range specified in the struct
+			json_object *jarray_chan = NULL;
+			// Get the channels array and look for the correct parameter
 			for(int j = 0; j < config_var.env_chan; j++){
+				if(!json_object_object_get_ex(jboard,channel_array_name,&jarray_chan)) {
+					LOG_PRINTF("The channel array %s is not present in the board %s in the configuration file\n",channel_array_name,config_var.board);
+					json_object_put(jarray_chan);
+					continue;
+				}
+				if(!json_object_array_get_idx(jarray_chan, j)) {
+					LOG_PRINTF("The channel %d is not present in the board %s in the configuration file\n",j,config_var.board);
+					json_object_put(jarray_chan);
+					continue;
+				}
+				if(!json_object_object_get_ex(jboard,config_var.json_word,&jmag)) {
+					LOG_PRINTF("The word %s is not present in the board %s in the configuration file\n",config_var.json_word,config_var.board);
+					json_object_put(jarray_chan);
+					continue;
+				}
+				mag_value = json_object_get_string(jmag);
+				if(mag_value == NULL) {
+					LOG_PRINTF("The value of %s in the board %s is not present in the configuration file\n",config_var.json_word,config_var.board);
+					json_object_put(jarray_chan);
+					continue;
+				}
+			//Concatenate the channel number up to the range specified in the struct
 				sprintf(cmd,"%s_%d_%s",config_var.magnitude,j,mag_value);
 				response = command_parse(cmd);
 				if(strcmp(response,"OK")){
 					LOG_PRINTF("Error setting %s in the board %s\n",cmd,config_var.board);
+					json_object_put(jarray_chan);
+					continue;
 				}
 				else{
-					DEBUG_PRINTF_1("Setting %s in the board %s\n",cmd,config_var.board);
+					DEBUG_PRINTF_1("OK Setting %s in the board %s\n",cmd,config_var.board);
+					json_object_put(jarray_chan);
+					continue;
 				}
+				json_object_put(jarray_chan);
+			}
+
+		}
+		else{ //Environmental variable
+			if(!json_object_object_get_ex(jboard,config_var.json_word,&jmag)) {
+				LOG_PRINTF("The word %s is not present in the board %s in the configuration file\n",config_var.json_word,config_var.board);
+				continue;
+			}
+			mag_value = json_object_get_string(jmag);
+			if(mag_value == NULL) {
+				LOG_PRINTF("The value of %s in the board %s is not present in the configuration file\n",config_var.json_word,config_var.board);
+				continue;
+			}
+			sprintf(cmd,"%s_%s",config_var.magnitude,mag_value);
+			response = command_parse(cmd);
+			if(strcmp(response,"OK")){
+				LOG_PRINTF("Error setting %s in the board %s\n",cmd,config_var.board);
+				continue;
+			}
+			else{
+				DEBUG_PRINTF_1("OK Setting %s in the board %s\n",cmd,config_var.board);
+				continue;
 			}
 		}
 		json_object_put(jboard);
@@ -4819,6 +4857,9 @@ int dig_command_translation(char *digcmd, char **cmd, int words_n){
 
 		// Setting pedestal type per channel
 		case HKDIG_SET_PED_ENABLE:
+
+		// Setting Channel control
+		case HKDIG_SET_CHN_CNTRL:
 
 		value1 = atoi(cmd[3]);
 		value2 = atoi(cmd[4]);
