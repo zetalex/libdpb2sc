58d680d401c458479fb2d1968f0645e7ef029324
Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>
2026-02-13 13:18:23 +0100

    Add command validation flag
    Make command validation optional
    
    Signed-off-by: Alejandro Gomez Gambin <alejandro.gomez-gambin@cern.ch>

--- dpb2sc_Cpp/src/dpb2sc.cpp
+++ dpb2sc_Cpp/src/dpb2sc.cpp
@@ -3816,9 +3816,12 @@ char* command_parse(const char *key){
 	}
 	//Check JSON schema valid
 	serialized_json = json_object_to_json_string(jobj);
-	rc = json_schema_validate("JSONSchemaCommandRequest.json",serialized_json, "cmd_temp.json");
-	if(rc){
-		rc = command_status_response_json (msg_id,-EINCMD,reply);
+	if(command_validation_flag){
+		rc = json_schema_validate("JSONSchemaCommandRequest.json",serialized_json, "cmd_temp.json");
+		if(rc){
+			rc = command_status_response_json (msg_id,-EINCMD,reply);
+			goto send_msg;
+		}
 	}
 	else{
 		char board_response[64];
@@ -4024,7 +4027,7 @@ char* command_parse(const char *key){
 		}
 	}
 	// Free JSON objects after using them
-	json_object_put(jobj);
+	send_msg: json_object_put(jobj);
 	// For DAQ, we are only interested in the msg_value
 	#ifdef DAQ_MODE
 		json_object * jcmd;
